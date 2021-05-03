a3797b2d665f42cdce3effedbda936c21c3b7e97
chrystalme <afam_ifeanyi@live.com>
2021-05-04 00:58:07 +0300

    Move functions to module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import {
-  pageLoad, menuPage, homeInfo, contactPage,
-} from './page_load';
+import { pageLoad } from './page_load';
+import { homeInfo } from './home_info';
+import { contactPage } from './contact_page';
+import { menuPage } from './menu_page';
 
+const ghostDiv = document.getElementsByClassName('.content');
 pageLoad();
 
+if (ghostDiv.childNodes[0]) {
+  ghostDiv.removeChild(ghostDiv.childNodes[0]);
+}
 const contact = document.getElementById('contact');
 const menu = document.getElementById('menu');
 const home = document.getElementById('home');
